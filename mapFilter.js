91424639b9a6af29fa3d09221a1105566f0e215d
unknown <coldplanet336@gmail.com>
2021-03-16 23:38:03 -0700

    map,filter,find

---
+++ mapFilter.js
@@ -0,0 +1,37 @@
+const num=[4,5,6,7,8]
+// const output=[]
+
+
+// for(let i=0;i<num.length;i++){
+//     const element = num[i]
+//     const result = element*element
+//     output.push(result)
+// }
+
+
+// function square(element){
+//     return element*element
+// }
+
+// const square = element => element* element
+// const square = x => x*x
+
+// const result = num.map(function(element){
+//     return element*element
+// })
+// const result = num.map(x=>x*x)
+// console.log(result)
+
+//filter...
+const bigger = num.filter(x=>x<5)
+console.log(bigger)
+
+
+
+
+
+
+
+
+
+
