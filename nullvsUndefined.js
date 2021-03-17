9eb07ded6ee6cdc759d388afd6640600086bf7a2
unknown <coldplanet336@gmail.com>
2021-03-16 21:13:06 -0700

    null vs undefined

---
+++ nullvsUndefined.js
@@ -0,0 +1,26 @@
+// undefined....
+// let pakhi
+// function add(num1,num2) {
+//     console.log(num1+num2)
+//     return
+// }
+// function add(num1,num2) {
+//     console.log(num1,num2)
+// }
+// let result=add(12)
+// 
+// let fun=undefined
+// console.log(fun)
+// ages=[11,12,13]
+// console.log(ages[11])
+
+
+// null......
+
+
+
+
+
+
+
+
