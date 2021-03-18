2b43b6acb684dc82541d64d0d1d763e2d3745f81
unknown <coldplanet336@gmail.com>
2021-03-18 01:13:33 -0700

    scope learning

---
+++ scope.js
@@ -0,0 +1,20 @@
+const bonus = 20
+function add(first,second){
+    let sum=first+second+bonus
+    // console.log(bonus)
+    if (sum>9) {
+        var mood = "happy"
+        mood = "sad"
+        mood = "exited"
+        mood = "funny"
+        // console.log(mood)
+    }
+    console.log(m)
+    let m="tonmoy"
+    // var m="tonmoy"
+    // console.log(m)
+    return sum
+}
+const result=add(5,7)
+// console.log(bonus)
+// console.log(result)
