b472326e62b785ad5b4ca24edef87f5709a30a6a
unknown <coldplanet336@gmail.com>
2021-03-18 16:33:22 -0700

    slice splice and join

---
+++ slice.js
@@ -0,0 +1,13 @@
+const nums = [1,2,3,4,5,6,7,8,9]
+// const part = nums.slice(2,6)
+const deleted = nums.splice(2,3, 314,169,152)
+// console.log(deleted)
+// console.log(nums)
+const sum = nums.join(" ")
+console.log(sum)
+
+
+
+
+
+
